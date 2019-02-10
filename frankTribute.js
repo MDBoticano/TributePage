6ba3059d8c42737e49d61440a3a80edb4010248c
Marlo <mdboticano@gmail.com>
2019-02-10 00:57:21 -0800

    Even more functionaltity of hover interaction with background

--- frankTribute.js
+++ frankTribute.js
@@ -12,13 +12,33 @@ $(document).ready(function(){
 
     // Hover over Channel Orange
     $(function() {
-    $('#channel-orange-art').hover(function() {
-        $('.channel-orange-songs').css('color', 'yellow');
-    }, function() {
-        // on mouseout, reset the background colour
-        $('.channel-orange-songs').css('color', '');
-        });
+        $('#channel-orange-art').hover(function() {
+            $('.channel-orange-songs').css('color', 'yellow');
+        }, function() {
+            // on mouseout, reset the background colour
+            $('.channel-orange-songs').css('color', '');
+            });
     });
 
+    // Hover over Endless
+    $(function() {
+        $('#endless-art').hover(function() {
+            $('.endless-songs').css('color', 'yellow');
+        }, function() {
+            // on mouseout, reset the background colour
+            $('.endless-songs').css('color', '');
+        });
+    });
+    
+    // Hover over Blonde
+    $(function() {
+        $('#blonde-art').hover(function() {
+            $('.blonde-songs').css('color', 'yellow');
+        }, function() {
+            // on mouseout, reset the background colour
+            $('.blonde-songs').css('color', '');
+        });
+    });
+    
 
 });
