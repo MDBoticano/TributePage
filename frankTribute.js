d1f469a4060b58646357747b33db5383fc8102c0
Marlo <mdboticano@gmail.com>
2019-02-10 00:24:23 -0800

    basic functionality of hover background implemented via jQuery

--- frankTribute.js
+++ frankTribute.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
+    // Hover over Nostalgia, Ultra
+    $(function() {
+        $('#nostalgia-ultra-art').hover(function() {
+          $('.hello').css('color', 'yellow');
+        }, function() {
+          // on mouseout, reset the background colour
+          $('.hello').css('color', '');
+        });
+      });
+
+    // Hover over Channel Orange
+    $(function() {
+    $('#channel-orange-art').hover(function() {
+        $('#bye').css('color', 'yellow');
+    }, function() {
+        // on mouseout, reset the background colour
+        $('#bye').css('color', '');
+        });
+    });
 
 
 });
