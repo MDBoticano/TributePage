2e222223f48641344a7eb6fc47ddcb4cd98d06a3
MDBoticano <MDBoticano@users.noreply.github.com>
2019-02-10 16:42:06 -0800

    Cleanup before major background changes

--- frankTribute.js
+++ frankTribute.js
@@ -1,44 +1,42 @@
 $(document).ready(function(){
-
-    // Hover over Nostalgia, Ultra
-    $(function() {
-        $('#nostalgia-ultra-art').hover(function() {
-          $('.nostalgia-ultra-songs').css('color', 'yellow');
-        }, function() {
-          // on mouseout, reset the background colour
-          $('.nostalgia-ultra-songs').css('color', '');
-        });
+  /* Code found on StackExchange, credit unknown sorry */
+  // Hover over Nostalgia, Ultra
+  $(function() {
+      $('#nostalgia-ultra-art').hover(function() {
+        $('.nostalgia-ultra-songs').css('color', 'yellow');
+      }, function() {
+        // on mouseout, reset the background colour
+        $('.nostalgia-ultra-songs').css('color', '');
       });
-
-    // Hover over Channel Orange
-    $(function() {
-        $('#channel-orange-art').hover(function() {
-            $('.channel-orange-songs').css('color', 'yellow');
-        }, function() {
-            // on mouseout, reset the background colour
-            $('.channel-orange-songs').css('color', '');
-            });
     });
 
-    // Hover over Endless
-    $(function() {
-        $('#endless-art').hover(function() {
-            $('.endless-songs').css('color', 'yellow');
-        }, function() {
-            // on mouseout, reset the background colour
-            $('.endless-songs').css('color', '');
-        });
-    });
-    
-    // Hover over Blonde
-    $(function() {
-        $('#blonde-art').hover(function() {
-            $('.blonde-songs').css('color', 'yellow');
-        }, function() {
-            // on mouseout, reset the background colour
-            $('.blonde-songs').css('color', '');
-        });
-    });
-    
+  // Hover over Channel Orange
+  $(function() {
+      $('#channel-orange-art').hover(function() {
+          $('.channel-orange-songs').css('color', 'yellow');
+      }, function() {
+          // on mouseout, reset the background colour
+          $('.channel-orange-songs').css('color', '');
+          });
+  });
 
+  // Hover over Endless
+  $(function() {
+      $('#endless-art').hover(function() {
+          $('.endless-songs').css('color', 'yellow');
+      }, function() {
+          // on mouseout, reset the background colour
+          $('.endless-songs').css('color', '');
+      });
+  });
+  
+  // Hover over Blonde
+  $(function() {
+      $('#blonde-art').hover(function() {
+          $('.blonde-songs').css('color', 'yellow');
+      }, function() {
+          // on mouseout, reset the background colour
+          $('.blonde-songs').css('color', '');
+      });
+  });
 });
